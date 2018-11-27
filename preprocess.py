b1c7dad6c66aa513a9803ef7499a77c0721e4bc3
KunchiLiu <983391357@qq.com>
2018-11-27 15:10:39 +0800

    Add data process

--- preprocess.py
+++ preprocess.py
@@ -10,7 +10,7 @@ class Channel:
 
     def process(self):
         print('Process users data...')
-        self._process_ratings_data()
+        self._process_users_data()
         print('Process movies data...')
         self._process_movies_data()
         print('Process ratings data...')
@@ -20,7 +20,7 @@ class Channel:
     def _process_users_data(self, file = 'users.dat'):
         f = pd.read_table(self.orign_path.format(file), sep='::', engine='python',
                           names=['userID', 'Gender', 'Age', 'Occupation', 'Zip-code'])
-        f.to_csv(self.orign_path.format('user.csv'), index=False)
+        f.to_csv(self.orign_path.format('users.csv'), index=False)
 
     def _process_ratings_data(self, file = 'ratings.dat'):
         f = pd.read_table(self.orign_path.format(file), sep='::', engine='python',
